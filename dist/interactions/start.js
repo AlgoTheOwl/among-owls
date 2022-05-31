e8b1f065e8085736c314a4a4f9ae3eb7df77e1fa
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-05-31 19:23:11 -0400

    add gifs

--- dist/interactions/start.js
+++ dist/interactions/start.js
@@ -22,12 +22,12 @@ async function startGame(interaction, hp, imageDir) {
     const players = (await database_service_1.collections.yaoPlayers
         .find({})
         .toArray());
-    if (players.length < 2) {
-        return await interaction.reply({
-            content: 'There are not enough players to start the game',
-            ephemeral: true,
-        });
-    }
+    // if (players.length < 2) {
+    //   return await interaction.reply({
+    //     content: 'There are not enough players to start the game',
+    //     ephemeral: true,
+    //   })
+    // }
     await interaction.deferReply();
     const gamePlayers = {};
     // empty image directory
