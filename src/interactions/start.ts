78b0b338e514798299a26b4309ef69bf7b10e543
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-06-01 09:05:33 -0400

    uncoment logic for not enough players

--- src/interactions/start.ts
+++ src/interactions/start.ts
@@ -29,12 +29,12 @@ export default async function startGame(
     .find({})
     .toArray()) as WithId<Player>[]
 
-  // if (players.length < 2) {
-  //   return await interaction.reply({
-  //     content: 'There are not enough players to start the game',
-  //     ephemeral: true,
-  //   })
-  // }
+  if (players.length < 2) {
+    return await interaction.reply({
+      content: 'There are not enough players to start the game',
+      ephemeral: true,
+    })
+  }
 
   await interaction.deferReply()
 
@@ -80,6 +80,7 @@ export default async function startGame(
     fields: mapPlayersForEmbed(playerArr),
   }
 
+  // TODO: fill out intor embed
   const intorEmbedData: EmbedData = {
     title: 'test',
   }
