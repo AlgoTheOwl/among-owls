65d156221e20b22caaa8a3e5f1dd74640cd9a2f5
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-06-21 13:16:45 -0400

    move attack command to commands attack folder

--- dist/commands/attack/index.js
+++ dist/commands/attack/index.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const builders_1 = require("@discordjs/builders");
-const attack_1 = require("./attack/attack");
+const attack_1 = require("./attack");
 module.exports = {
     data: new builders_1.SlashCommandBuilder()
-        .setName('random-attack')
+        .setName('attack')
         .setDescription('Attack another user!'),
     async execute(interaction) {
         if (!interaction.isButton())
             return;
-        (0, attack_1.attack)(interaction, true);
+        (0, attack_1.attack)(interaction, false);
     },
 };
