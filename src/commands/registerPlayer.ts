5d0f3dc2fbb911a4aa2cd3c1bf0f28ec5f40200c
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-06-27 23:08:48 -0400

    Add player list to waiting room,  refactor embed logic

--- src/commands/registerPlayer.ts
+++ src/commands/registerPlayer.ts
@@ -14,56 +14,60 @@ module.exports = {
     .setName('register-player')
     .setDescription('Register an active player'),
   async execute(interaction: SelectMenuInteraction) {
-    if (!interaction.isSelectMenu()) return
-    if (!game.waitingRoom) return
-
-    const { values, user } = interaction
-    const { username, id } = user
-    const { imageDir, hp, messageDeleteInterval } = settings
-
-    const { assets, address, _id } = (await collections.users.findOne({
-      discordId: user.id,
-    })) as WithId<User>
-
-    const asset = assets.find((asset) => asset.assetId === Number(values[0]))
-
-    if (!asset) {
-      return
-    }
-
-    let localPath
-
     try {
-      localPath = await downloadFile(asset, imageDir, username)
+      if (!interaction.isSelectMenu()) return
+      if (!game.waitingRoom) return
+
+      const { values, user } = interaction
+      const { username, id } = user
+      const { imageDir, hp, messageDeleteInterval } = settings
+
+      const { assets, address, _id } = (await collections.users.findOne({
+        discordId: user.id,
+      })) as WithId<User>
+
+      const asset = assets.find((asset) => asset.assetId === Number(values[0]))
+
+      if (!asset) {
+        return
+      }
+
+      let localPath
+
+      try {
+        localPath = await downloadFile(asset, imageDir, username)
+      } catch (error) {
+        console.log('download error', error)
+      }
+
+      if (!localPath) {
+        return
+      }
+
+      const gameAsset = new Asset(
+        asset.assetId,
+        asset.assetName,
+        asset.assetUrl,
+        asset.unitName,
+        _id,
+        localPath
+      )
+
+      game.players[id] = new Player(
+        username,
+        id,
+        address,
+        gameAsset,
+        _id,
+        hp,
+        assets.length,
+        0
+      )
+      interaction.reply(`${asset.assetName} has entered the game`)
+      await wait(messageDeleteInterval)
+      interaction.deleteReply()
     } catch (error) {
-      console.log('download error', error)
+      console.log(error)
     }
-
-    if (!localPath) {
-      return
-    }
-
-    const gameAsset = new Asset(
-      asset.assetId,
-      asset.assetName,
-      asset.assetUrl,
-      asset.unitName,
-      _id,
-      localPath
-    )
-
-    game.players[id] = new Player(
-      username,
-      id,
-      address,
-      gameAsset,
-      _id,
-      hp,
-      assets.length,
-      0
-    )
-    interaction.reply(`${asset.assetName} has entered the game`)
-    await wait(messageDeleteInterval)
-    interaction.deleteReply()
   },
 }
