c40833e738384d25e0e3c521f01bf865175b2445
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-06-28 23:50:48 -0400

    fix clear timeout script

--- src/commands/clearTimeout.ts
+++ src/commands/clearTimeout.ts
@@ -1,8 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { Interaction } from 'discord.js'
-import { game } from '..'
-import Player from '../models/player'
-import { asyncForEach } from '../utils/helpers'
 import { collections } from '../database/database.service'
 
 module.exports = {
@@ -11,16 +8,11 @@ module.exports = {
     .setDescription('clear all timeouts'),
   async execute(interaction: Interaction) {
     if (!interaction.isCommand()) return
-    const playerArr = Object.values(game.players)
 
     await interaction.deferReply({ ephemeral: true })
 
-    asyncForEach(playerArr, async (player: Player) => {
-      await collections.users.findOneAndUpdate(
-        { _id: player.userId },
-        { $set: { coolDownDone: null } }
-      )
-    })
+    await collections.users.updateMany({}, { $set: { coolDownDone: null } })
+
     return await interaction.editReply('Timeouts cleared')
   },
 }
