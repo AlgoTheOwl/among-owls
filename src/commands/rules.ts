2da50fb7261fe6afb5ed8e651d21fc9085a9f021
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-07-30 17:57:50 -0400

    add opt in asset id to rules

--- src/commands/rules.ts
+++ src/commands/rules.ts
@@ -10,10 +10,11 @@ module.exports = {
   async execute(interaction: Interaction) {
     if (!interaction.isCommand()) return
 
+    const optInAssetId = process.env.OPT_IN_ASSET_ID
+
     return interaction.reply({
       ephemeral: true,
-      content:
-        'GAME RULES:\n- Opt into asset ID: 410471638\n- /register in bot-cmd discord channel\n- Game auto-starts when lobby fills with 4 AOWLs\n- Random damage\n- 30 minute cooldown for each AOWL after battle\n-Winner awarded 5 $HOOT per battle, just type /claim after your victory\n- Slash commands /register, /leaderboard, /rename',
+      content: `GAME RULES:\n- Opt into asset ID: ${optInAssetId}\n- /register in bot-cmd discord channel\n- Game auto-starts when lobby fills with 4 AOWLs\n- Random damage\n- 30 minute cooldown for each AOWL after battle\n-Winner awarded 5 $HOOT per battle, just type /claim after your victory\n- Slash commands /register, /leaderboard, /rename`,
     })
   },
 }
