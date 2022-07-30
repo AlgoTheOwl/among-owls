09cb73d8c276083455600cca50177df13f8f6fa4
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-07-30 17:17:59 -0400

    add /rules command

---
+++ src/commands/rules.ts
@@ -0,0 +1,18 @@
+// Discord
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { Interaction } from 'discord.js'
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('rules')
+    .setDescription('see game rules'),
+  async execute(interaction: Interaction) {
+    if (!interaction.isCommand()) return
+
+    return interaction.reply({
+      ephemeral: true,
+      content:
+        'GAME RULES:\n- Opt into asset ID: 410471638\n- /register in bot-cmd discord channel\n- Game auto-starts when lobby fills with 4 AOWLs\n- Random damage\n- 30 minute cooldown for each AOWL after battle\n-Winner awarded 5 $HOOT per battle, just type /claim after your victory\n- Slash commands /register, /leaderboard, /rename',
+    })
+  },
+}
