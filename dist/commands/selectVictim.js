f2e8b34d2756d08e17056c6e62eec490dad38753
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-08-03 20:01:52 -0400

    Fix victimId bug not setting

--- dist/commands/selectVictim.js
+++ dist/commands/selectVictim.js
@@ -9,29 +9,22 @@ module.exports = {
         .setName('select-victim')
         .setDescription('Choose a new victim to attack'),
     async execute(interaction) {
-        if (!__1.game)
-            return;
-        const { values: idArr, user } = interaction;
-        const victimId = idArr[0] || null;
-        if (!victimId) {
-            return interaction.editReply({
-                content: 'Something went wrong, please try again',
+        if (!__1.game.active || __1.game.waitingRoom) {
+            return interaction.reply({
+                content: 'There is no active game to select a victim',
+                ephemeral: true,
             });
         }
+        const { values: idArr, user } = interaction;
+        const victimId = idArr[0];
         const player = __1.game.players[user.id];
+        const victim = __1.game.players[victimId];
         if (victimId === 'random') {
             if (player)
                 player.victimId = undefined;
             return interaction.reply({
                 ephemeral: true,
                 content: `You have chosen to attack a random player`,
-            });
-        }
-        const victim = __1.game.players[victimId];
-        if (!__1.game.active || __1.game.waitingRoom) {
-            return interaction.reply({
-                content: 'There is no active game to select a victim',
-                ephemeral: true,
             });
         }
         if (player && victim) {
@@ -41,15 +34,11 @@ module.exports = {
                     ephemeral: true,
                 });
             }
-            if (player === null || player === void 0 ? void 0 : player.victimId) {
-                player.victimId = victimId;
-            }
-            if (victim) {
-                return interaction.reply({
-                    ephemeral: true,
-                    content: `You have chosen ${victim.username}'s AOWL to be your victim, good choice. 😈`,
-                });
-            }
+            player.victimId = victimId;
+            return interaction.reply({
+                ephemeral: true,
+                content: `You have chosen ${victim.username}'s AOWL to be your victim, good choice. 😈`,
+            });
         }
         else {
             return interaction.reply({
